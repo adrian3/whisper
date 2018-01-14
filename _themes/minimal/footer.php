a7dfd685b1caa733eb2a1e7451f49fc95890ef31
Adrian Hanft <adrianhanft@gmail.com>
2018-01-14 09:49:23 -0700

    Fixing broken paths

--- _themes/minimal/footer.php
+++ _themes/minimal/footer.php
@@ -5,7 +5,7 @@
 </div>
 
 <footer>
-  <small style="color: #ccc;">&copy; <?php echo $copyright; ?> &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp; <a style="color: #ccc;" href="<?php echo $siteUrl; ?>/humans.txt">humans.txt</a> &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp; <a style="color: #ccc;" href="<?php echo $siteUrl; ?>/sitemap.xml">sitemap.xml</a> &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp; <a style="color: #ccc;" href="https://adrian3.github.io/steam/">steam powered</a></small>
+  <small style="color: #ccc;">&copy; <?php echo $copyright; ?> &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp; <a style="color: #ccc;" href="<?php echo $siteUrl; ?>/humans.txt">humans.txt</a> &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp; <a style="color: #ccc;" href="<?php echo $siteUrl; ?>/sitemap.xml">sitemap.xml</a> &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp; <a style="color: #ccc;" href="https://adrian3.github.io/whisper/">powered by a whisper</a></small>
 </footer>
 
 <script type="text/javascript">
